6d90a781b0179292d06711696ea5761701bd0214
00bam <bam3322tty@gmail.com>
2024-02-28 11:02:36 -0800

    debugging

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,7 +17,11 @@ int main(void)
 			if (i % 3 == 0)
 				printf("Fizz");
 			if (i % 5 == 0)
-				printf("Buzz ");
+			{
+				printf("Buzz");
+				if (i != 100)
+					printf(" ");
+			}
 			else
 				printf(" ");
 		}
