4aac7504c9036a2ae19b7303612846451e8ed1b9
00bam <bam3322tty@gmail.com>
2024-03-05 08:35:37 -0800

    trial

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,14 +7,14 @@
  */
 char *cap_string(char *str)
 {
-	char *ptr = str, *separators_pointer;
-	char *separators = ",;.!?\"({} \n\t";
+	char *ptr = str, *s_pointer;
+	char *s = ",;.!?\"({} \n\t";
 
 	for (; *str != '\0'; str++)
 	{
-		for (separators_pointer = separators; *separators_pointer != '\0'; separators_pointer++)
+		for (s_pointer = s; *s_pointer != '\0'; s_pointer++)
 		{
-			if (*str == *separators_pointer)
+			if (*str == *s_pointer)
 			{
 				if (str[1] <= 122 && str[1] >= 97)
 					str[1] = str[1] - 32;
